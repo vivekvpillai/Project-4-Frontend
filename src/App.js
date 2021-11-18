61f50c5ec79df417e5d5e640620588e5c9a8fbb7
Vijayan <vijayan@Vijayans-MBP.attlocal.net>
2021-11-17 19:49:15 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+
+// import Add from './components/Add'
+// import Edit from './components/Edit'
+
+function App() {
+  let [entries, setEntries] = useState([])
+
+  const getEntries = () => {
+    axios
+      .get('http://localhost:8000/api/foods')
+      .then(
+        (response)=> setEntries(response.data),
+        (err) => console.error(err)
+      )
+      .catch((error) => console.error(error))
+  }
+
+  const handleCreate = (addEntry) => {
+    axios
+      .post('http://localhost:8000/api/foods', addEntry).then((response) => {
+        console.log(response)
+        getEntries()
+      })
+  }
+
+  const handleDelete = (event) => {
+    axios
+      .delete('http://localhost:8000/api/foods/' + event.target.value)
+      .then((response) => {
+        getEntries()
+      })
+  }
+
+  const handleUpdate = (editEntry) => {
+    axios
+      .put('http://localhost:8000/api/foods/' + editEntry.id, editEntry)
+      .then((response) => {
+        getEntries()
+      })
+  }
+
+  useEffect(() => {
+    getEntries()
+  }, [])
+
+  return (
+    <>
+    <h1>Hello</h1>
+    <div className="entries">
+      {entries.map((entry) => {
+        return(
+          <div className="entry" key={entry.id}>
+            <h4>Name: {entry.name}</h4>
+            <br/>
+            <img src={entry.image} />
+            <br/>
+            <h4>Calories: {entry.calories}</h4>
+            <br/>
+          </div>
+        )
+      })}
+    </div>
+    </>
+  );
+}
+
+export default App;
